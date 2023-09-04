9b778f313ed843e9ecb551cb1f7202850bb88f22
elvis <comando@duocuc.cl>
2023-09-03 22:50:32 -0300

    campo contraseña validada en TS

--- TallerApp/src/app/login/login.page.ts
+++ TallerApp/src/app/login/login.page.ts
@@ -2,19 +2,34 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  templateUrl:'./login.page.html',
+  templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
 
 export class LoginPage implements OnInit {
-
   contrasena: string = '';
+  mostrarMensaje: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  
-  
+  validarContrasena() {
+    if (this.contrasena === '') {
+      // El campo de contraseña está vacío, no mostrar el mensaje de validación
+      this.mostrarMensaje = false;
+    } else {
+      const regex = /^(?=.*[0-9].*[0-9].*[0-9].*[0-9])(?=.*[A-Z])(?=.*[a-zA-Z].*[a-zA-Z].*[a-zA-Z])([a-zA-Z0-9]+)$/;
+
+      if (regex.test(this.contrasena)) {
+        // La contraseña cumple con los requisitos, ocultar el mensaje de validación
+        this.mostrarMensaje = false;
+      } else {
+        // La contraseña no cumple con los requisitos, mostrar el mensaje de validación
+        this.mostrarMensaje = true;
+      }
+    }
+  }
 }
+
