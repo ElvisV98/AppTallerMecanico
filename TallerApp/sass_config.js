bf8c13f7e9938167e60ebc3f7f62ef4033bad39f
Dani-XT <danielte98@gmail.com>
2024-01-12 16:09:30 -0300

    se arregla problemas con colores

--- TallerApp/sass_config.js
+++ TallerApp/sass_config.js
@@ -4,6 +4,5 @@ module.export = {
         "node_modules/ionicons/scss",
         "node_modules/ionic-angular/fonts",
         "node_modules/bootstrap/scss",
-        "node_modules/bootstrap/js"
     ]
 }
